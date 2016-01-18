eba7d03ab3e7a9024d467ae7b1b664d38d023728
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2016-01-18 21:54:10 +0530

    Initial Commit

---
+++ SimpleTableViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTableViewExample
+//
+//  Created by Maitrayee Ghosh on 18/01/16.
+//  Copyright © 2016 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTableViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTableViewExample
+//
+//  Created by Maitrayee Ghosh on 18/01/16.
+//  Copyright © 2016 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
