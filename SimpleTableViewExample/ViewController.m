d35aa541b103998dae1025319eb549758970b982
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2016-01-19 22:53:05 +0530

    SimpleUITableViewExample
    Simple example of UITableView

--- SimpleTableViewExample/ViewController.m
+++ SimpleTableViewExample/ViewController.m
@@ -16,7 +16,62 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    dataArray=[[NSArray alloc]initWithObjects:@"Apple",
+                                              @"Mango",
+                                              @"Papaya",
+                                              @"Guava",
+                                              @"Pineapple",
+                                              @"Strawberry",
+                                              @"Banana",
+                                              nil];
+    
+    self.mTableView.delegate=self;
+    self.mTableView.dataSource=self;
+}
+
+#pragma mark - UITableView DataSource Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [dataArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = [dataArray objectAtIndex:indexPath.row];
+    return cell;
+}
+
+#pragma mark - UITableView Delegate Methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *message=[NSString stringWithFormat:@"User selected %@",[dataArray objectAtIndex:indexPath.row]];
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:@"iOSTute"
+                                          message:message
+                                          preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *okAction = [UIAlertAction
+                               actionWithTitle:@"OK"
+                               style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction *action)
+                               {
+                                   NSLog(@"OK action");
+                               }];
+    
+    [alertController addAction:okAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
